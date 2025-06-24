236808a6b6b5fecf684a1ffdc2c8aabdc9d6642b
Fransei29 <seilerfranco317@gmail.com>
2025-06-24 11:06:03 -0300

    Fixing General Styles For Franklin Web.

--- components/LoadingSpinner/LoadingSpinnerComponent.tsx
+++ components/LoadingSpinner/LoadingSpinnerComponent.tsx
@@ -1,11 +1,19 @@
 // components/LoadingSpinner.tsx
 import { Loader2 } from 'lucide-react' // o cualquier otro ícono
 import styles from './LoadingSpinner.module.scss' 
+import Image from 'next/image';
 
 export default function LoadingSpinner() {
   return (
     <div className={styles.loadingOverlay}>
       <Loader2 className={styles.spinner} />
+      <Image
+          src="/dev3.png"
+          alt="Logo"
+          width={55}
+          height={55}
+          className={styles.logo}
+        />
     </div>
   )
 }
