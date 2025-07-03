2598c93ac423487e1fbc08ef5c067ef2d848eece
Fransei29 <seilerfranco317@gmail.com>
2025-07-03 19:19:50 -0300

    Updating General for version 2

--- app/components/Hero.tsx
+++
@@ -1,8 +0,0 @@
-        background: linear-gradient(
-          to top,
-          rgba(0, 0, 0, 0.9) 0%,
-          rgba(0, 0, 0, 0.7) 30%,
-          rgba(0, 0, 0, 0.4) 60%,
-          rgba(0, 0, 0, 0.2) 80%,
-          rgba(0, 0, 0, 0.0) 100%
-        ); 
