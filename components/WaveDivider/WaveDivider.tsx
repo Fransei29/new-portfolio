50c1b85cd50572a24ee07fea143c39a7c6a8f8de
Fransei29 <seilerfranco317@gmail.com>
2026-03-24 19:46:57 -0300

    update driver view details/ deleted dead code

--- components/WaveDivider/WaveDivider.tsx
+++ components/WaveDivider/WaveDivider.tsx
@@ -9,12 +9,18 @@ export default function WaveDivider({ from, to }: WaveDividerProps) {
       <svg
         viewBox="0 0 1440 52"
         preserveAspectRatio="none"
-        style={{ width: '100%', height: '52px', display: 'block' }}
+        style={{ width: '100%', height: '52px', display: 'block', overflow: 'hidden' }}
         aria-hidden="true"
       >
+        <defs>
+          <filter id="waveDepth" x="0%" y="-20%" width="100%" height="140%">
+            <feDropShadow dx="0" dy="8" stdDeviation="8" floodColor="rgba(0,0,0,0.22)" />
+          </filter>
+        </defs>
         <path
           d="M0,26 C480,52 960,0 1440,26 L1440,52 L0,52 Z"
           fill={to}
+          filter="url(#waveDepth)"
         />
       </svg>
     </div>
